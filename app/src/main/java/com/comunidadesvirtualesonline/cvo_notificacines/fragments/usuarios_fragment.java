f9cc29e97ade1694bd03a9a2e1fed4ca17f8e202
daniel1622 <danielpa06@gmail.com>
2016-01-12 20:44:35 -0500

    creacion de layouts de registro de ususarios y inicio de sesion

--- app/src/main/java/com/comunidadesvirtualesonline/cvo_notificacines/fragments/usuarios_fragment.java
+++ app/src/main/java/com/comunidadesvirtualesonline/cvo_notificacines/fragments/usuarios_fragment.java
@@ -3,11 +3,20 @@ package com.comunidadesvirtualesonline.cvo_notificacines.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.comunidadesvirtualesonline.cvo_notificacines.R;
+import com.comunidadesvirtualesonline.cvo_notificacines.adapters.EstudianteAdapter;
+import com.comunidadesvirtualesonline.cvo_notificacines.adapters.UsuariosAdapter;
+import com.comunidadesvirtualesonline.cvo_notificacines.models.Estudiante;
+import com.comunidadesvirtualesonline.cvo_notificacines.models.Usuarios;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +34,28 @@ public class usuarios_fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_usuarios, container, false);
+    }
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ArrayList<Usuarios> UsuariosArray = new ArrayList<>();
+
+        Usuarios usuario1 = new Usuarios();
+
+        usuario1.setNomEstudiante("DANIEL PARDO CUENCA");
+        usuario1.setCurEstudiante("Octavo");
+        usuario1.setIdEstudiante("12345678");
+
+        UsuariosArray.add(usuario1);
+
+        RecyclerView recyclerView = (RecyclerView) getActivity().findViewById(R.id.my_recycler_view_2);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(new UsuariosAdapter(UsuariosArray, R.layout.row_usuarios));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
     }
 
 }
